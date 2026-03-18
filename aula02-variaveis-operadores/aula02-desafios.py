e900a82b520866ec7bf2485025876abf094b5b36
jucalemos <jucalemossouza@gmail.com>
2026-03-18 20:47:14 -0300

    aula 2 variaveis

---
+++ aula02-variaveis-operadores/aula02-desafios.py
@@ -0,0 +1,19 @@
+# DESAFIO 1
+
+nome = input("Digite seu nome:")
+print("Bem vindo", nome)
+
+# DESAFIO 2
+
+dia = input("Digite o dia do seu nascimento:")
+mes = input("Digite o mês do seu nascimento:")
+ano = input("Digite o ano do seu nascimento:")
+
+print(f"Sua data de nascimento: {dia}/{mes}/{ano}")
+
+# DESAFIO 3
+
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+
+print(f"Soma: {num1+num2}")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,32 @@
+print('ola mundo')
+
+print(7+4)
+print("7 + 4")
+print("7" + "4") #CONCATENANDO
+
+# Comentário de 1 linha
+'''
+Comentários de 
+múltiplas 
+linhas
+'''
+
+# VARIÁVEIS
+nome = "Rodrigo" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print("Oiiii", nome, idade, peso)
+print(f"Oiiii {nome}!!!")
+
+# INPUT _ SIMULAÇÂO DE UM FORMS NO CMD
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+peso = float(input("Digite seu peso:"))
+
+print (nome, idade, peso)
+print (idade + 1)
+
+ano_nascimento =1976
+ano_atual =2026
+print(ano_atual-ano_nascimento)
